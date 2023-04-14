bc70e87e1058953aca20606bc1f6943ffd91f1b4
dd1014 <minidy346@naver.com>
2023-04-14 16:35:21 +0900

    db/schema.sql 추가

---
+++ src/main/java/com/kdy/exam/project/service/ArticleService.java
@@ -0,0 +1,38 @@
+package com.kdy.exam.project.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.kdy.exam.project.repository.ArticleRepository;
+import com.kdy.exam.project.vo.Article;
+
+@Service
+public class ArticleService {
+	private ArticleRepository articleRepository;
+
+	public ArticleService(ArticleRepository articleRepository) {
+		this.articleRepository = articleRepository;
+		articleRepository.makeTestData();
+	}
+
+	public Article getArticle(int id) {
+		return articleRepository.getArticle(id);
+	}
+
+	public List<Article> getArticles() {
+		return articleRepository.getArticles();
+	}
+
+	public Article writeArticle(String title, String body) {
+		return articleRepository.writeArticle(title, body);
+	}
+
+	public void deleteArticle(int id) {
+		articleRepository.deleteArticle(id);
+	}
+
+	public void modifyArticle(int id, String title, String body) {
+		articleRepository.modifyArticle(id, title, body);
+	}
+}
