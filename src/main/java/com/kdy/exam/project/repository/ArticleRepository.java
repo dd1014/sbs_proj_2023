76bc0ef9b299693ce96a14555bceb687528833dd
dd1014 <minidy346@naver.com>
2023-04-14 18:18:51 +0900

    INSERT 쿼리를 메서드와 연결, LAST_INSERT_ID 사용

--- src/main/java/com/kdy/exam/project/repository/ArticleRepository.java
+++ src/main/java/com/kdy/exam/project/repository/ArticleRepository.java
@@ -3,6 +3,7 @@ package com.kdy.exam.project.repository;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -18,13 +19,16 @@ public interface ArticleRepository {
 	@Select("SELECT * FROM article ORDER BY id DESC")
 	public List<Article> getArticles();
 	
-	
-	public Article writeArticle(String title, String body);
+	@Insert("INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = #{title}, `body` = #{body}")
+	public void writeArticle(@Param("title") String title, @Param("body") String body);
 	
 	@Delete("DELETE FROM article where id = #{id}")
 	public void deleteArticle(@Param("id") int id);
 	
-	@Update("UPDATE article SET title = #{title}, `body` = #{body}, updateDate = NOW WHERE id = #{id}")
+	@Update("UPDATE article SET title = #{title}, `body` = #{body}, updateDate = NOW() WHERE id = #{id}")
 	public void modifyArticle(@Param("id") int id, @Param("title") String title, @Param("body") String body);
+	
+	@Select("SELECT LAST_INSERT_ID()")
+	public int getLastInsertId();
 
 }
