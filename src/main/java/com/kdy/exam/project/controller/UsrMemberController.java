1d2367cee5a9a650d6ca704c577b83d86ce5021a
dd1014 <minidy346@naver.com>
2023-04-18 16:47:20 +0900

    로그인 기능 구현

--- src/main/java/com/kdy/exam/project/controller/UsrMemberController.java
+++ src/main/java/com/kdy/exam/project/controller/UsrMemberController.java
@@ -12,6 +12,8 @@ import com.kdy.exam.project.utill.Ut;
 import com.kdy.exam.project.vo.Member;
 import com.kdy.exam.project.vo.ResultData;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class UsrMemberController {
 	// 인스턴스 변수 시작
@@ -68,19 +70,55 @@ public class UsrMemberController {
 		if (Ut.empty(email)) {
 			return ResultData.from("F-6", "email(을)를 입력해주세요.");
 		}
-		
-		//S-1
-		//회원가입이 완료되었습니다.
-		//7(몇번회원인지)
+
+		// S-1
+		// 회원가입이 완료되었습니다.
+		// 7(몇번회원인지)
 		ResultData<Integer> joinRd = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+
+		if (joinRd.isFail()) {
+			return (ResultData) joinRd;
+		}
+
+		Member Member = memberService.getMember(joinRd.getData1());
+
+		return ResultData.newData(joinRd, Member);
+	}
+
+	@RequestMapping("/usr/member/doLogin")
+	@ResponseBody
+	public ResultData<Member> doJoin(HttpSession httpSession, String loginId, String loginPw) {
+		boolean isLogined = false;
 		
-		if( joinRd.isFail()) {
-			return (ResultData)joinRd;
+		if(httpSession.getAttribute("loginedMemberId") != null) {
+			isLogined = true;
 		}
 		
-		Member Member = memberService.getMember(joinRd.getData1());
+		if( isLogined ) {
+			return ResultData.from("F-5", "이미 로그인되었습니다.");
+		}
 		
-		return ResultData.newData(joinRd, Member);
+		if (Ut.empty(loginId)) {
+			return ResultData.from("F-1", "loginId(을)를 입력해주세요.");
+		}
+		if (Ut.empty(loginPw)) {
+			return ResultData.from("F-2", "loginPw(을)를 입력해주세요.");
+		}
+		
+		Member member = memberService.getMemberByLoginId(loginId);
+		
+		if( member == null ) {
+			return ResultData.from("F-3", "존재하지 않은 로그인아이디 입니다.");
+		}
+		
+		if( member.getLoginPw().equals(loginPw) == false ) {
+			return ResultData.from("F-4", "비밀번호가 일치하지 않습니다.");
+		}
+		
+		httpSession.setAttribute("loginedMemberId", member.getLoginId());
+		
+		//로그인성공시
+		return ResultData.from("S-1", Ut.f("%s님 환영합니다.", member.getNickname()));
 	}
 
 	@RequestMapping("/usr/member/doDelete")
