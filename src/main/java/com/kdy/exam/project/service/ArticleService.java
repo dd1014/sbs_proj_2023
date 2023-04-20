8204ed5f76afcf35640ca7b56e16099a53e5d9ee
dd1014 <minidy346@naver.com>
2023-04-20 16:03:30 +0900

    출력용 게시물 가져올 떄, 액터 정보 넘겨서 더욱 자세한 정보 생성하기

--- src/main/java/com/kdy/exam/project/service/ArticleService.java
+++ src/main/java/com/kdy/exam/project/service/ArticleService.java
@@ -17,13 +17,46 @@ public class ArticleService {
 		this.articleRepository = articleRepository;
 	}
 
-	public Article getForPrintArticle(int id) {
+	public Article getForPrintArticle(int actorId, int id) {
+		Article article = articleRepository.getForPrintArticle(actorId);
+		
+		updateForPrintData(actorId, article);
+		
 		return articleRepository.getForPrintArticle(id);
 	}
 
-	public List<Article> getForPrintArticles() {
-		return articleRepository.getForPrintArticles();
+	public List<Article> getForPrintArticles(int actorId) {
+		List<Article> articles = articleRepository.getForPrintArticles();
+		
+		for(Article article : articles ) {
+			updateForPrintData(actorId, article);
+		}
+		
+		return articles;
 	}
+	
+	private void updateForPrintData(int actorId, Article article) {
+		if (article == null ){
+			return;
+		}
+		
+		ResultData actorCanDeleteRd = actorCanDelete(actorId, article);
+		//삭제할수잇으면 얘맞아 심어주기
+		article.setExtra_actorCanDelete(actorCanDeleteRd.isSuccess());
+		
+	}
+	public ResultData actorCanDelete(int actorId, Article article) {
+		if( article == null ) {
+			return ResultData.from("F-1", "게시물이 존재하지 않습니다.");
+		}
+		if( article.getMemberId() != actorId ) {
+			return ResultData.from("F-2", "권한이 없습니다.");
+		}
+		
+		return ResultData.from("S-1", "게시물 삭제가 가능합니다.");
+		
+	}
+	
 
 	public ResultData<Integer> writeArticle(int memberId, String title, String body) {
 		articleRepository.writeArticle(memberId, title, body);
@@ -40,7 +73,7 @@ public class ArticleService {
 		articleRepository.modifyArticle(id, title, body);
 	
 	
-		Article article = getForPrintArticle(id);
+		Article article = getForPrintArticle(0, id);
 		
 		return ResultData.from("S-1", Ut.f("%d번 게시물이 수정되었습니다.",	id), "article", article);
 	}
