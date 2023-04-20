d7daeda4a2346b489b199a157b46fc88b560df6f
dd1014 <minidy346@naver.com>
2023-04-20 17:47:25 +0900

    Rq객체를 BeforAction인터셉터에서 대표로 한번만 만들기

--- src/main/java/com/kdy/exam/project/interceptor/BeforeActionInterceptor.java
+++ src/main/java/com/kdy/exam/project/interceptor/BeforeActionInterceptor.java
@@ -3,6 +3,8 @@ package com.kdy.exam.project.interceptor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import com.kdy.exam.project.vo.Rq;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -12,7 +14,8 @@ public class BeforeActionInterceptor implements HandlerInterceptor{
 	//이런식으로 쓰면 인터셉터를 구현한거다라고 생각하기 (이해노)
 	@Override
 	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
-		System.out.println("실행가능?");
+		Rq rq = new Rq(req);
+		req.setAttribute("rq", rq);
 
 		return HandlerInterceptor.super.preHandle(req, resp, handler);
 	}
