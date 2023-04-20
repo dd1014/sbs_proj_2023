534d9f1d8bb363ac29518f832f18d3e0abab31d4
dd1014 <minidy346@naver.com>
2023-04-20 17:34:54 +0900

    BeforeActionInterceptor 등록

--- src/main/java/com/kdy/exam/project/MyWebMvcConfigurer.java
+++ src/main/java/com/kdy/exam/project/MyWebMvcConfigurer.java
@@ -2,6 +2,7 @@ package com.kdy.exam.project;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.kdy.exam.project.interceptor.BeforeActionInterceptor;
@@ -14,7 +15,11 @@ public class MyWebMvcConfigurer implements WebMvcConfigurer{
 	@Autowired
 	BeforeActionInterceptor berforeActionInterceptor;
 	
-	
+	// 이 함수는 인터셉터를 적용하는 역할을 합니다.
+		@Override
+		public void addInterceptors(InterceptorRegistry registry) {
+			registry.addInterceptor(berforeActionInterceptor).addPathPatterns("/**").excludePathPatterns("/error");
+		}
 	
 	
 }
