c0c7143ccd3ecbf6addfb08b86b6e5dca829da47
dd1014 <minidy346@naver.com>
2023-04-28 14:26:59 +0900

    댓글 삭제

--- src/main/java/com/kdy/exam/project/controller/UsrReplyController.java
+++ src/main/java/com/kdy/exam/project/controller/UsrReplyController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.kdy.exam.project.service.ReplyService;
 import com.kdy.exam.project.utill.Ut;
+import com.kdy.exam.project.vo.Reply;
 import com.kdy.exam.project.vo.ResultData;
 import com.kdy.exam.project.vo.Rq;
 
@@ -22,31 +23,61 @@ public class UsrReplyController {
 	@RequestMapping("/usr/reply/doWrite")
 	@ResponseBody
 	String doWrite(String relTypeCode, int relId, String body, String replaceUri) {
-		if(Ut.empty(relTypeCode) ) {
+		if (Ut.empty(relTypeCode)) {
 			return rq.jsHistoryBack("relTypeCode(을)를 입력해주세요.");
 		}
 
-		if(Ut.empty(relId) ) {
+		if (Ut.empty(relId)) {
 			return rq.jsHistoryBack("relId(을)를 입력해주세요.");
 		}
-		
-		if(Ut.empty(body) ) {
+
+		if (Ut.empty(body)) {
 			return rq.jsHistoryBack("body(을)를 입력해주세요.");
 		}
-		
+
 		ResultData<Integer> writeReplyRd = replyService.writeReply(rq.getLoginedMemberId(), relTypeCode, relId, body);
-		
+
 		int id = writeReplyRd.getData1();
-		
-		if(Ut.empty(replaceUri)) {
-			switch(relTypeCode) {
+
+		if (Ut.empty(replaceUri)) {
+			switch (relTypeCode) {
 			case "article":
 				replaceUri = Ut.f("../article/detail?id=%d", relId);
 				break;
 			}
 		}
-		
+
 		return rq.jsReplace(writeReplyRd.getMsg(), replaceUri);
+	}
+
+	@RequestMapping("/usr/reply/doDelete")
+	@ResponseBody
+	String doDelete(int id, String replaceUri) {
+		if (Ut.empty(id)) {
+			return rq.jsHistoryBack("relTypeCode(을)를 입력해주세요.");
+		}
+
+		Reply reply = replyService.getForPrintReply(rq.getLoginedMemberId(), id);
+
+		if (reply == null) {
+			return rq.jsHistoryBack(Ut.f("%d번 댓글이 존재하지 않습니다.", id));
+		}
+		
+		if (reply.isExtra_actorCanDelete() == false) {
+			return rq.jsHistoryBack(Ut.f("%d번 댓글을 삭제할 권한이 없습니다.", id));
+		}
+		
+		ResultData deleteReplyRd = replyService.deleteReply(id);
+		
+		if (Ut.empty(replaceUri)) {
+			switch (reply.getRelTypeCode()) {
+			case "article":
+				replaceUri = Ut.f("../article/detail?id=%d", reply.getRelId());
+				break;
+			}
+		}
+
+		return rq.jsReplace(deleteReplyRd.getMsg(), replaceUri);
 	}
 
 }
--- src/main/java/com/kdy/exam/project/service/ReplyService.java
+++ src/main/java/com/kdy/exam/project/service/ReplyService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import com.kdy.exam.project.repository.ReplyRepository;
 import com.kdy.exam.project.utill.Ut;
+import com.kdy.exam.project.vo.Article;
 import com.kdy.exam.project.vo.Reply;
 import com.kdy.exam.project.vo.ResultData;
 
@@ -25,10 +26,63 @@ public class ReplyService {
 	}
 
 	public List<Reply> getForPrintReplies(int actortId, String relTypeCode, int relId) {
+		List<Reply> replies = replyRepository.getForPrintReplies(actortId, relTypeCode, relId);
+		
+		for(Reply reply : replies) {
+			updateForPrintData(actortId, reply);
+		}
+		
+		return replies;
+	}
 
-		return replyRepository.getForPrintReplies(actortId, relTypeCode, relId);
+	private void updateForPrintData(int actorId, Reply reply) {
+		if (reply == null) {
+			return;
+		}
+
+		ResultData actorCanDeleteRd = actorCanDelete(actorId, reply);
+		reply.setExtra_actorCanDelete(actorCanDeleteRd.isSuccess());
+
+		ResultData actorCanModifyRd = actorCanModify(actorId, reply);
+		reply.setExtra_actorCanModify(actorCanModifyRd.isSuccess());
 	}
 	
+	public ResultData actorCanModify(int actorId, Reply reply) {
+		if (reply == null) {
+			return ResultData.from("F-1", "댓글이 존재하지 않습니다.");
+		}
+
+		if (reply.getMemberId() != actorId) {
+			return ResultData.from("F-2", "권한이 없습니다.");
+		}
+
+		return ResultData.from("S-1", "댓글 수정이 가능합니다.");
+	}
+	public ResultData actorCanDelete(int actorId, Reply reply) {
+		if (reply == null) {
+			return ResultData.from("F-1", "댓글이 존재하지 않습니다.");
+		}
+		
+		if (reply.getMemberId() != actorId) {
+			return ResultData.from("F-2", "권한이 없습니다.");
+		}
+		
+		return ResultData.from("S-1", "댓글 삭제가 가능합니다.");
+	}
+
+	public Reply getForPrintReply(int actorId, int id) {
+		Reply reply = replyRepository.getForPrintReply(actorId, id);
+		
+		updateForPrintData(actorId, reply);
+		
+		return reply;
+	}
+
+	public ResultData deleteReply(int id) {
+		replyRepository.deleteReply(id);
+		
+		return ResultData.from("S-1", Ut.f("%d번 댓글을 삭제하였습니다.", id));
+	}
 	
 	
 }
