b7abde62b34035a4963ca449fd77583575a34434
dd1014 <minidy346@naver.com>
2023-05-02 15:47:24 +0900

    로그아웃 상태에서 작성 버튼 누르면, 로그인 하라는 메세지와 함께 로그인화면으로 이동

--- src/main/java/com/kdy/exam/project/interceptor/NeedLoginInterceptor.java
+++ src/main/java/com/kdy/exam/project/interceptor/NeedLoginInterceptor.java
@@ -23,7 +23,7 @@ public class NeedLoginInterceptor implements HandlerInterceptor {
 		// rq가 너 로그인햇냐 물어보기
 
 		if (!rq.isLogined()) {
-			rq.printHistoryBackjs("로그인 후 이용해주세요.");
+			rq.printReplaceJs("로그인 후 이용해주세요.", "../member/login");
 			return false;
 		}
 
--- src/main/java/com/kdy/exam/project/vo/Rq.java
+++ src/main/java/com/kdy/exam/project/vo/Rq.java
@@ -119,6 +119,11 @@ public class Rq {
 		
 		
 	}
+
+	public void printReplaceJs(String msg, String uri) {
+		resp.setContentType("text/html; charset=UTF-8");
+		print(Ut.jsReplace(msg, uri));
+	}
 	
 	
 }
